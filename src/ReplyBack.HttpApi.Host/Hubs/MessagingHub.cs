25f614fe9975a46b3f16520d77410027f9fa0e95
Sturla Thorvaldsson <sturla@ibeinni.is>
2022-04-21 19:42:53 +0000

    A working example

---
+++ src/ReplyBack.HttpApi.Host/Hubs/MessagingHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using Volo.Abp.AspNetCore.SignalR;
+
+namespace BSR.Beinni.Hubs
+{
+    [Authorize]
+    public class MessagingHub : AbpHub
+    {
+        public async Task SendMessage(string message)
+        {
+            message = $"{CurrentUser.UserName}: {message}";
+
+            await Clients
+                .User(CurrentUser.Id.ToString())
+                .SendAsync("MessageBackToBlazor", message);
+
+            //This works for sending to ALL
+            //await Clients.All.SendAsync("MessageBackToBlazor", message);
+        }
+    }
+}
